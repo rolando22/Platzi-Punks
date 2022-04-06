d071d3ac678d94c024f18d33fa55cd14ccdc55d1
rolando22 <rolando2207@gmail.com>
2022-04-05 23:07:59 -0300

    Agrego scripts para el deploy del contrato

---
+++ contracts/PlatziPunks.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract PlatziPunks is ERC721 {
+
+    constructor() ERC721("PlatziPunk", "PLPKS") {
+
+    }
+}
